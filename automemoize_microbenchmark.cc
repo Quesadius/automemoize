9901c4c8c76155a1e904c52ca7ada1e5bc50a1b4
Joe Atkins <cheese@Enduring-Hawk.local>
2025-11-19 22:15:50 -0600

    clang-format C++ code.

--- automemoize_microbenchmark.cc
+++ automemoize_microbenchmark.cc
@@ -1,25 +1,26 @@
-#include "automemoize.h"
 #include <benchmark/benchmark.h>
+
+#include "automemoize.h"
 
 // Naive recursive Fibonacci
 int fibonacci(int n) {
-    if (n <= 1) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 static void BM_Fibonacci_Naive(benchmark::State& state) {
-    for (auto _ : state) {
-        benchmark::DoNotOptimize(fibonacci(state.range(0)));
-    }
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(fibonacci(state.range(0)));
+  }
 }
 BENCHMARK(BM_Fibonacci_Naive)->Arg(20);
 
 // Memoized Fibonacci
 static void BM_Fibonacci_Memoized(benchmark::State& state) {
-    auto memoized_fib = automemoize(fibonacci);
-    for (auto _ : state) {
-        benchmark::DoNotOptimize(memoized_fib(state.range(0)));
-    }
+  auto memoized_fib = automemoize(fibonacci);
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(memoized_fib(state.range(0)));
+  }
 }
 BENCHMARK(BM_Fibonacci_Memoized)->Arg(20);
 
